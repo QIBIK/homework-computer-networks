6073f7188cbdca4d0d9b070ca0a6b4e79977306b
QIBIK <59399311+QIBIK@users.noreply.github.com>
2025-02-11 00:28:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
 #include <fstream>
 #include <thread>
+#include <string>
 
 using namespace std;
 
 long getFileSize(const string& filename) {
-    ifstream file(filename, ios::binary | ios::ate); // Открываем файл в бинарном режиме и переводим каретку в конец
-    if (!file.is_open()) { // Проверяем, открыт ли файл
+    ifstream file(filename, ios::binary | ios::ate);
+    if (!file.is_open()) {
         return -1;
     }
-    return file.tellg(); // Возвращаем текующую позицию курсора и это будет наш размера файла
+    return file.tellg();
+}
+
+string readFileContents(const string& filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        return "";
+    }
+    string contents((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    return contents;
 }
 
 int main() {
     setlocale(LC_ALL, "rus");
-    string filename = "test.txt";
+    string filename = "C:/Users/Andrew/Desktop/test.txt";
 
     long last_size = getFileSize(filename);
+    string last_content = readFileContents(filename);
+
     if (last_size == -1) {
         cerr << "Ошибка: файл не найден!" << endl;
         return 1;
@@ -26,11 +38,23 @@ int main() {
 
     while (true) {
         long current_size = getFileSize(filename);
+        string current_content = readFileContents(filename);
+
         if (current_size != last_size) {
             cout << "Файл изменился! Новый размер: " << current_size << " байт" << endl;
+
+            if (current_content.size() > last_content.size()) {
+                cout << "Добавлено: " << current_content.substr(last_content.size()) << endl;
+            }
+            else if (current_content.size() < last_content.size()) {
+                cout << "Удалено: " << last_content.substr(current_content.size()) << endl;
+            }
+
             last_size = current_size;
+            last_content = current_content;
         }
-        this_thread::sleep_for(chrono::milliseconds(500)); // Проверка каждые 500 мс
+
+        this_thread::sleep_for(chrono::milliseconds(500));
     }
 
     return 0;
